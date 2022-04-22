7e2ba4d8d9327c5ef33d1bf010e90d5657be9084
jacver <jacobvernau@gmail.com>
2022-04-21 21:12:12 -0600

    new seed format prevents 1-off date between html and db docs

--- controllers/dateControllers.js
+++ controllers/dateControllers.js
@@ -1,25 +1,26 @@
 // controllers/dateControllers.js
 
 // DEPENDENCIES
-const express = require('express');
+const express = require("express");
 const dateRouter = express.Router();
-const Resort = require('../models/resort');
-const SlopeDay = require('../models/slopeDay');
-const ejs = require('ejs');
+const Resort = require("../models/resort");
+const SlopeDay = require("../models/slopeDay");
+const ejs = require("ejs");
+const resortRouter = require("./resortControllers");
 
 // HELPER FUNCTIONS
 
-function formatDate(date) {
-  // change from UNIX to JS Date OBJ
-  const dateObj = new Date(date);
-  // MM-DD-YYYY
-  dateObj.getMonth() +
-    1 +
-    '-' +
-    dateObj.getDate() +
-    '-' +
-    dateObj.getFullYear();
-}
+// function formatDate(date) {
+//   // change from UNIX to JS Date OBJ
+//   const dateObj = new Date(date);
+//   // MM-DD-YYYY
+//   dateObj.getMonth() +
+//     1 +
+//     "-" +
+//     dateObj.getDate() +
+//     "-" +
+//     dateObj.getFullYear();
+// }
 
 // START DATE ROUTE CONTROLLERS *
 // the following routes are prepended with /dates
@@ -31,26 +32,33 @@ function formatDate(date) {
 //   });
 // });
 
-// // show specific dates at a specific resort
-dateRouter.get('/:formattedDate', (req, res) => {
+// // show all same-dates at a specific resort
+// dateRouter.get("/:formattedDate", (req, res) => {
+// eventually URL should read resorts/:resortName/:date
+// // this GET should then display all documents with matching DATE and RESORT NAME on a card
+
+// const formattedDate = req.params.formattedDate;
+
+dateRouter.get("/:formattedDate", (req, res) => {
   const formattedDate = req.params.formattedDate;
 
-  SlopeDay.find({}) // isolate dates
-    .then((dateObj) => {
-      dateObj.forEach((date, i) => {
-        let dateStr =
-          // MM-DD-YYYY reformatting to string
-          ('0' + (date.date.getMonth() + 1)).slice(-2) +
-          '-' +
-          ('0' + date.date.getDate()).slice(-2) +
-          '-' +
-          date.date.getFullYear();
-        const objWithFormattedDate = dateObj.map((obj) => {
-          return { ...obj, formattedDate: dateStr, isNew: true };
-        });
-        console.log(objWithFormattedDate);
-      });
-    });
+  // break up the date
+  const targetChars = formattedDate.substring(3, 5);
+  const beforeTargetChar = formattedDate.substring(0, 3);
+  const afterTargetChar = formattedDate.substring(5);
+
+  // create lower and upper boundaries that straddle the formatted date
+  const lowerbound = beforeTargetChar + (targetChars - 1) + afterTargetChar;
+  const upperbound =
+    beforeTargetChar + (Number(targetChars) + 1) + afterTargetChar;
+
+  SlopeDay.find({
+    date: {
+      // find docs with dates between the boundaries (THIS SHOULD EQUAL req.params.formattedDate)
+      $gte: new Date(lowerbound),
+      $lt: new Date(upperbound),
+    }, // add 2nd query here
+  }).then((dateData) => res.send(dateData));
 });
 
 // END DATE ROUTE CONTROLLERS *
--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 // index.js
 
-require('dotenv').config(); // import dotenv to hide mongoURI from github
+require("dotenv").config(); // import dotenv to hide mongoURI from github
 
-const express = require('express'); // import express
+const express = require("express"); // import express
 
 const app = express(); // establish app object
 
-const methodOverride = require('method-override'); // import method override for route queries
+const methodOverride = require("method-override"); // import method override for route queries
 
 const PORT = process.env.PORT || 3000; // heroku-friendly port
 
 // import controllers
 // ** IMPORTANT ** - dateControllers must be above resortControllers or it will get hung up on the resort get /:id route
-const dateControllers = require('./controllers/dateControllers');
-const resortControllers = require('./controllers/resortControllers');
+const dateControllers = require("./controllers/dateControllers");
+const resortControllers = require("./controllers/resortControllers");
 
-app.use(methodOverride('_method')); // utilize method override
-app.use(express.static('public'));
+app.use(methodOverride("_method")); // utilize method override
+app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/resorts', resortControllers);
-app.use('/date', dateControllers);
+app.use("/resorts", resortControllers);
+app.use("/date", dateControllers);
 
-app.set('view engine', 'ejs'); // view engine EJS for templating
+app.set("view engine", "ejs"); // view engine EJS for templating
 
 app.listen(PORT, () => {
-  console.log('App listening on PORT ', PORT);
+  console.log("App listening on PORT ", PORT);
 });
